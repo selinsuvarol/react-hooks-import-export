39d08e49533e7fb6a3657ada8c22a2a019ac7c8c
Selin Su Varol <selinsuvarol@gmail.com>
2022-07-07 10:31:43 +0300

    completed codealong

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,7 +1,7 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
 
-function ColoradoStateParks() {
+export default function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
   return <h1>Colorado State Parks!</h1>;
